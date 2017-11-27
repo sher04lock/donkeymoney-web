416d5079368fffa5c6a8e903c8c1fe799fa5e421
Bartek Grodek <bartek@enterpryze.com>
2017-11-27 21:09:38 +0100

    add login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '../authentication.service';
 import { User } from '../user';
 import { ActivatedRoute, Router } from '@angular/router';
+import { browser } from 'protractor';
+
+// tslint:disable-next-line:max-line-length
+const loadingGif = "data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==";
 
 @Component({
   selector: 'app-login',
@@ -10,6 +14,8 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
   user: User = new User();
+  pendingRequest = false;
+  loadingGif = loadingGif;
 
   constructor(
     private authenticationService: AuthenticationService,
@@ -17,13 +23,14 @@ export class LoginComponent implements OnInit {
     private router: Router,
   ) { }
 
-
-
   onLogin(): void {
+    this.pendingRequest = true;
     this.authenticationService.login(this.user)
-      .subscribe(() => this.router.navigate(["/dashboard"]));
+      .subscribe(() => {
+        this.router.navigate(["/dashboard"]);
+        this.pendingRequest = false;
+      }, (error) => { console.log(error); this.pendingRequest = false; });
   }
-
 
   ngOnInit() {
     const returnUrl = this.activatedRoute.snapshot.queryParams['returnUrl'] || '/';
