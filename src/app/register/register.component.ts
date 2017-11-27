bb7aba1560565ce47bfe09ff61622e6664445fb3
Bartek Grodek <bartek@enterpryze.com>
2017-11-27 21:10:10 +0100

    add register component

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -3,6 +3,8 @@ import { User } from '../user';
 import { AuthenticationService } from '../authentication.service';
 import { ActivatedRoute, Router } from '@angular/router';
 
+// tslint:disable-next-line:max-line-length
+const loadingGif = "data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==";
 
 @Component({
   selector: 'app-register',
@@ -11,17 +13,26 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class RegisterComponent implements OnInit {
 
+  pendingRequest = false;
+  loadingGif = loadingGif;
   user = new User();
+
   constructor(private authenticationService: AuthenticationService,
     private activatedRoute: ActivatedRoute,
     private router: Router
   ) { }
 
   onRegister() {
+    this.pendingRequest = true;
     this.authenticationService
       .register(this.user)
-      .subscribe(() => this.router.navigate(["/login"]));
+      .subscribe(() => { this.router.navigate(["/login"]); this.pendingRequest = false; },
+      error => {
+        console.log(error);
+        this.pendingRequest = false;
+      });
   }
+
   ngOnInit() {
     const returnUrl = this.activatedRoute.snapshot.queryParams['returnUrl'] || '/';
     if (this.authenticationService.isAuthenticated()) {
@@ -30,5 +41,3 @@ export class RegisterComponent implements OnInit {
   }
 
 }
-
-
